3250f8b8bca10cb9ca6e1414cd388e1bd275c0b3
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2024-12-19 16:30:39 +0100

    switch to release-24.11 as latest stable

--- versions.nix
+++ versions.nix
@@ -5,7 +5,7 @@ rec {
   sha = with builtins; fromJSON (readFile ./versions.json);
 
   # latest, stable release string (without the dot):
-  stable = "2405";
+  stable = "2411";
 
   getNixpkgs = ver: import ( builtins.fetchTarball
     "https://github.com/NixOS/nixpkgs/archive/${sha."${ver}".nixpkgs}.tar.gz");
